1353151cc1a270e66bc2e5387fa8d8cbc2884212
ranaasamir <36300706+ranaasamir@users.noreply.github.com>
2018-02-28 21:56:37 +0200

    Add files via upload

--- front-end/src/components/Dashboard/Views/Overview.vue
+++ front-end/src/components/Dashboard/Views/Overview.vue
@@ -34,7 +34,8 @@
             info: 'SE is fun!  okaayyy...not so much',
             image: 'static/img/members/seif.jpg',
             social: 'https://twitter.com/seifhatem'
-          },{
+          },
+          {
             name: 'Meray Samy',
             tag: 'meraysamy',
             info: 'BI Student at GUC',
@@ -63,20 +64,20 @@
             name: 'Rana Samir',
             tag: 'Ranasamir',
             info: 'My zodiac sign is Aquarius',
-            image: 'static/img/members/rana.jpg'
+            image: 'static/img/faces/rana.jpg'
           },
           {
             name: 'Robert Shenouda',
             tag: 'RobertShenouda',
             info: 'A lead guitarist who loves rock music like who doesnt???',
-            image: 'static/img/members/robert.jpg',
-
+            image: 'static/img/members/robert.jpg'
           },
           {
-            name: 'Farah Ahmed',
-            tag: 'farahmed',
-            info: 'Everything will pass',
-            image: 'static/img/members/Farah.jpg'
+            name: 'PUT YOUR NAME HERE',
+            tag: 'NAMEHEREWITHOUTSPACES',
+            info: 'DO NOT DELETEE IT, COPY IT AND PUT IT ABOVE THIS',
+            image: 'static/img/members/delete.jpg',
+            social: 'https://twitter.com/seifhatem'
           }
         ]
       }
