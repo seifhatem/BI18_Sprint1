4abb5ff27b65d2819825de3c1bb337cf2e6dfec4
Seif Elmenabawy <Seif@Seifs-MacBook-Pro.local>
2018-03-02 21:02:11 +0200

    Login & Register backend

--- back-end/api/controllers/AuthenticationController.js
+++ back-end/api/controllers/AuthenticationController.js
@@ -12,23 +12,43 @@
         data: null
       });
     }
-     const users = await User.find({username: req.body.username}).exec();
+     const users = await User.find({username: req.body.username,password:req.body.password}).exec();
      if(users.length==1){
        res.status(200).json({
+         msg: 'Valid username & password'
        });
      }
 
      if(users.length==0){
-       res.status(403).json({
-         err: 'Invalid username or password'
+       res.status(200).json({
+         msg: 'Invalid username or password'
        });
      }
 
      if(users.length>1){
-       res.status(500).json({
-         err: 'Duplicate users found!'
+       res.status(200).json({
+         msg: 'Duplicate users found!'
        });
      }
 
 
 }
+
+module.exports.createUser = async (req, res) => {
+  if (!Validations.isString(req.body.username) || !Validations.isString(req.body.password) || !Validations.isString(req.body.email)) {
+    return res.status(422).json({
+      err: null,
+      msg: 'username, password & email required',
+      data: null
+    });
+  }
+
+  const user = await User.create(req.body);
+  return res.status(201).json({
+    err: null,
+    msg: 'User was created successfully.',
+    data: user
+  });
+
+
+};
